199ee4aa682214f554f75b7fb5fc96ca6984dc62
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-02-09 15:56:25 +0700

    add mock data

--- mysql/01_animal.sql
+++ mysql/01_animal.sql
@@ -1,7 +1,13 @@
 -- Create the animal table
 CREATE TABLE IF NOT EXISTS animal
 (
-    animal_id INT AUTO_INCREMENT,
+    animal_id INT          NOT NULL AUTO_INCREMENT,
     name      VARCHAR(255) NOT NULL,
     PRIMARY KEY (animal_id)
 );
+
+-- Insert mock data into the animal table
+INSERT INTO animal
+VALUES (1, 'หมา'),
+       (2, 'แมว'),
+       (3, 'กระต่าย');
--- mysql/02_symptom.sql
+++ mysql/02_symptom.sql
@@ -1,7 +1,13 @@
 -- Create the symptom table
 CREATE TABLE IF NOT EXISTS symptom
 (
-    symptom_id   INT AUTO_INCREMENT,
+    symptom_id   INT          NOT NULL AUTO_INCREMENT,
     symptom_name varchar(255) NOT NULL,
     PRIMARY KEY (symptom_id)
-)
+);
+
+-- Insert mock data into the symptom table
+INSERT INTO symptom
+VALUES (1, 'ท้องเสีย'),
+       (2, 'อาเจียน'),
+       (3, 'เบื่ออาหาร');
