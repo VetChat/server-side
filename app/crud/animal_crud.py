8fdcdff4b66824fc8255c5b80e5cd6d4f9144b6e
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-02-12 20:00:18 +0700

    add return type in crud

--- app/crud/animal_crud.py
+++ app/crud/animal_crud.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import Session
 from ..models import Animal
 
 
-def fetch_animal(db: Session):
+def fetch_animal(db: Session) -> List[Type[Animal]]:
     return db.query(Animal).all()
 
 
-def fetch_animal_by_id(db: Session, animal_id: int):
+def fetch_animal_by_id(db: Session, animal_id: int) -> Type[Animal]:
     return db.query(Animal).filter(Animal.id == animal_id).first()
