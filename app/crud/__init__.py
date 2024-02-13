db3f3866046930330ba555619cc8f3c255fbf184
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-02-13 19:51:42 +0700

    import schemas and add urgent crud

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,3 +1,4 @@
 from .animal_crud import AnimalCRUD
+from .urgent_crud import UrgentCRUD
 
-__all__ = ["AnimalCRUD"]
+__all__ = ["AnimalCRUD", "UrgentCRUD"]
---
+++ app/crud/urgent_crud.py
@@ -0,0 +1,23 @@
+from typing import Type, List, Optional
+from sqlalchemy.orm import Session
+from ..models import UrgentCase, Urgency
+
+
+class UrgentCRUD:
+    def __init__(self, db: Session) -> None:
+        self.db = db
+
+    def fetch_all_urgent_case(self):
+        return (
+            self.db.query(UrgentCase)
+            .join(Urgency, UrgentCase.urgency_id == Urgency.urgency_id)
+            .all()
+        )
+
+    def fetch_urgent_case_by_animal_id(self, animal_id: int):
+        return (
+            self.db.query(UrgentCase)
+            .join(Urgency, UrgentCase.urgency_id == Urgency.urgency_id)
+            .filter(UrgentCase.animal_id == animal_id)
+            .all()
+        )
