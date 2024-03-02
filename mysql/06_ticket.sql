58dec2a35cf671721492df009ba64fe07a9b93de
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-03-02 21:39:22 +0700

    add is_answered column

--- mysql/06_ticket.sql
+++ mysql/06_ticket.sql
@@ -8,13 +8,14 @@ CREATE TABLE IF NOT EXISTS ticket
     breed            VARCHAR(255) NOT NULL,
     birth_when       DATETIME     NOT NULL,
     rec_created_when DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    is_answered      INT          NOT NULL DEFAULT 0,
     PRIMARY KEY (ticket_id),
     CONSTRAINT FK_TicketAnimal FOREIGN KEY (animal_id) REFERENCES animal (animal_id)
 );
 
 -- Insert mock data to the ticket table
 INSERT INTO ticket
-VALUES (1, 1, 'male', 'sterile', 'Husky', '2012-03-07 00:00:00', '2024-02-06 15:35:44'),
-       (2, 1, 'male', 'sterile', 'Bull dog', '2024-11-24 00:00:00', '2024-02-07 08:12:35'),
-       (3, 1, 'female', 'non-sterile', 'Shiba', '2019-05-15 00:00:00', '2024-02-09 15:53:59');
+VALUES (1, 1, 'male', 'sterile', 'Husky', '2012-03-07 00:00:00', '2024-02-06 15:35:44', 1),
+       (2, 1, 'male', 'sterile', 'Bull dog', '2024-11-24 00:00:00', '2024-02-07 08:12:35', 1),
+       (3, 1, 'female', 'non-sterile', 'Shiba', '2019-05-15 00:00:00', '2024-02-09 15:53:59', 1);
 
