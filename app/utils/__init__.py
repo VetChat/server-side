8bded6d7e62d47400d6afc83278612cac670773a
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-04-08 02:48:44 +0700

    add more test and set target for codecov

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,4 @@
-from .duration_utils import DurationUtils
 from .limiter import limiter
 from .file_name import format_file_name
 
-__all__ = ['DurationUtils', 'limiter', 'format_file_name']
+__all__ = ['limiter', 'format_file_name']
