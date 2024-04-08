869e8b9b2805e176dddc4a1d3e7f468abfe34434
Chatchawarin Chatchavalwong <chatchawarin.chatchavalwong@agoda.com>
2024-04-08 16:02:09 +0700

    fix test s3

--- tests/aws/test_s3.py
+++ tests/aws/test_s3.py
@@ -1,3 +1,4 @@
+import pytest
 from unittest.mock import MagicMock, patch
 from fastapi import UploadFile
 from app.aws import S3Resource
@@ -11,23 +12,26 @@ def test_s3_upload_file_success():
         assert s3_resource.upload_file_to_s3(file, "dog", "cough", "question1") is not None
 
 
-def test_s3_upload_file_failure():
+@pytest.mark.asyncio
+async def test_s3_upload_file_failure():
     s3_resource = S3Resource()
     file = MagicMock(spec=UploadFile)
     file.filename = "test.jpg"
     with patch.object(s3_resource.s3, "upload_fileobj", side_effect=Exception):
-        assert s3_resource.upload_file_to_s3(file, "dog", "cough", "question1") is None
+        assert await s3_resource.upload_file_to_s3(file, "dog", "cough", "question1") is None
 
 
-def test_s3_remove_file_success():
+@pytest.mark.asyncio
+async def test_s3_remove_file_success():
     s3_resource = S3Resource()
     with patch.object(s3_resource.s3, "Object", return_value=MagicMock(delete=MagicMock(return_value=None))):
-        assert s3_resource.remove_file_from_s3(
+        assert await s3_resource.remove_file_from_s3(
             "https://vetchat.s3.region.amazonaws.com/dog_cough_question1.jpg") is True
 
 
-def test_s3_remove_file_failure():
+@pytest.mark.asyncio
+async def test_s3_remove_file_failure():
     s3_resource = S3Resource()
     with patch.object(s3_resource.s3, "Object", side_effect=Exception):
-        assert s3_resource.remove_file_from_s3(
+        assert await s3_resource.remove_file_from_s3(
             "https://vetchat.s3.region.amazonaws.com/dog_cough_question1.jpg") is False
